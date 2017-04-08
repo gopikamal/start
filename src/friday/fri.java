d9d5c5901bfa2231cc10d8c798575be3b80d084d
s <gopikamal638@gmail.com>
2017-04-08 06:55:18 +0530

    use ing for loop Change-Id: Ib049fe30a144005487a67918bb576e0d8bee6ce9 Signed-off-by: s <gopikamal638@gmail.com>

--- src/friday/fri.java
+++ src/friday/fri.java
@@ -4,9 +4,13 @@ public class fri {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		for (int i=0;i<=10;i++)
+			
+		{
+			
 	System.out.println("friday testing ");
 // system.out.println("testing friday");	
-		
+		}		
 	}
 
 }
